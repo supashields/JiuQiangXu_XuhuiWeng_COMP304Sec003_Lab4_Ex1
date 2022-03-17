43eeb3812db154f010884fffa2b1e231b1b887e4
John Xu <johnxu27@gmail.com>
2022-03-16 23:12:48 -0400

    3 Activities
    Yet to add proper content

--- app/src/main/java/com/example/jiuqiangxu_xuhuiweng_comp304sec003_lab4_ex1/MainActivity.java
+++ app/src/main/java/com/example/jiuqiangxu_xuhuiweng_comp304sec003_lab4_ex1/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.jiuqiangxu_xuhuiweng_comp304sec003_lab4_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void checkStockChart(View view) {
+        String type = new String();
+
+
+        Intent intent0 = new Intent(MainActivity.this,RestaurantListActivity.class);
+        intent0.putExtra("type",type);
+        startActivity(intent0);
     }
 }
