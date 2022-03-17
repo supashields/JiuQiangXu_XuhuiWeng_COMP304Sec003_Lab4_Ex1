af027baa1d450d663796e9b34a1010961cef557d
John Xu <johnxu27@gmail.com>
2022-03-16 20:32:45 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jiuqiangxu_xuhuiweng_comp304sec003_lab4_ex1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.jiuqiangxu_xuhuiweng_comp304sec003_lab4_ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
