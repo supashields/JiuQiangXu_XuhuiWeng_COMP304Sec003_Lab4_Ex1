26bcf5770ccb08122b5865b9c796efe8136e9dfd
John Xu <johnxu27@gmail.com>
2022-03-18 05:27:07 -0400

    Restaurant List Activity
    2 activities done

--- app/src/main/java/com/example/jiuqiangxu_xuhuiweng_comp304sec003_lab4_ex1/MainActivity.java
+++ app/src/main/java/com/example/jiuqiangxu_xuhuiweng_comp304sec003_lab4_ex1/MainActivity.java
@@ -3,26 +3,61 @@ package com.example.jiuqiangxu_xuhuiweng_comp304sec003_lab4_ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.CheckBox;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView listView;
+    private ArrayAdapter arrayAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void checkStockChart(View view) {
-        String type = new String();
+        Resources res = getResources();
+        String[] cuisine = res.getStringArray(R.array.cuisine);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, cuisine);
+        //create listview object
+        final ListView listView = (ListView)findViewById(R.id.listviewCuisine);
+        listView.setAdapter(adapter);
+        //set item clicked event
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                //intent for each item on list clicked
+                Intent intent0 = new Intent(MainActivity.this,RestaurantListActivity.class);
+//                String[] restaurants = res.getStringArray(R.array.chinese);
+                switch(position){
+                    case 0:
+                        intent0.putExtra("restaurants",res.getStringArray(R.array.chinese));
+                        break;
+                    case 1:
+                        intent0.putExtra("restaurants",res.getStringArray(R.array.italian));
+                        break;
+                    case 2:
+                        intent0.putExtra("restaurants",res.getStringArray(R.array.greek));
+                        break;
+                    case 3:
+                        intent0.putExtra("restaurants",res.getStringArray(R.array.indian));
+                        break;
+                    case 4:
+                        intent0.putExtra("restaurants",res.getStringArray(R.array.japanese));
+                        break;
+                }
+                startActivity(intent0);
 
 
-        Intent intent0 = new Intent(MainActivity.this,RestaurantListActivity.class);
-        intent0.putExtra("type",type);
-        startActivity(intent0);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/jiuqiangxu_xuhuiweng_comp304sec003_lab4_ex1/RestaurantListActivity.java
+++ app/src/main/java/com/example/jiuqiangxu_xuhuiweng_comp304sec003_lab4_ex1/RestaurantListActivity.java
@@ -2,13 +2,73 @@ package com.example.jiuqiangxu_xuhuiweng_comp304sec003_lab4_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class RestaurantListActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_list);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Bundle extras = getIntent().getExtras();
+        String[] restaurants = extras.getStringArray("restaurants");
+
+        Resources res = getResources();
+//        String[] restaurantslist = res.getStringArray(R.array.cuisine);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(RestaurantListActivity.this, android.R.layout.simple_list_item_1, restaurants);
+        //create listview object
+        final ListView listView = (ListView)findViewById(R.id.listviewRes);
+        listView.setAdapter(adapter);
+        //set item clicked event
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                //intent for each item on list clicked
+                Intent intent0 = new Intent(RestaurantListActivity.this,MapsActivity.class);
+                double lat = 43.6532;
+                double lng = -79.3832;
+                double[] coordinates = new double[] {lat, lng};
+//                for(int i = 0; i < restaurants.size(); i++){
+//
+//                }
+//                switch(position){
+//                    case 0:
+//                        intent0.putExtra("restaurants",res.getStringArray(R.array.chinese));
+//                        break;
+//                    case 1:
+//                        intent0.putExtra("restaurants",res.getStringArray(R.array.italian));
+//                        break;
+//                    case 2:
+//                        intent0.putExtra("restaurants",res.getStringArray(R.array.greek));
+//                        break;
+//                    case 3:
+//                        intent0.putExtra("restaurants",res.getStringArray(R.array.indian));
+//                        break;
+//                    case 4:
+//                        intent0.putExtra("restaurants",res.getStringArray(R.array.japanese));
+//                        break;
+//                }
+                startActivity(intent0);
+                intent0.putExtra("coordinates",coordinates);
+            }
+        });
     }
 }
